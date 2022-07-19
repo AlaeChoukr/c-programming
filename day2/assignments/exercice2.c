b1cf52fdc1432af9a8fe08699cc0b962e42f8ddb
AlaeChoukr <91984437+AlaeChoukr@users.noreply.github.com>
2022-07-19 13:27:21 +0100

    Create exercice2.c

---
+++ day2/assignments/exercice2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    printf("Befor the permutaion\na = %d\nb = %d\n",a,b);
+    c=a;
+    a=b;
+    b=c;
+    printf("After the permutation\na = %d\nb = %d\n",a,b);
+}
